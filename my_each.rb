44bbc05ece7a1d49b55491a85ff032d7aa56dbf6
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-18 21:44:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,2 @@
 
-things = ["ab", "cd", "ef"]
 
-def my_each(array)
-  i = 0
-  while i < array.length
-# The "while" method will always return a value of => nil"
-    yield(array[i])
-    i = i + 1
-  end
-end
-
-my_each(things) {|i| puts " #{i} hello"}
